20677bb76b935d13bb7d8e9eed2eb7e6dc5263ef
brian aljer <bcoballes@ateneo.edu>
2013-08-01 13:23:20 +0800

    MirageMod with Collapse/Expand Beta

---
+++ dspace-xmlui/src/main/webapp/themes/MirageMod/lib/treeview/treeview.js
@@ -0,0 +1,12 @@
+/* as suggested by Mr. Elvi Nemiz, modified by brian aljer 2013-8-1 */
+
+<script type="text/javascript">
+(function ($) {
+    $("#tree").treeview({
+        collapsed:true,
+        animated:"medium",
+        control:"#sidetreecontrol",
+        persist:"cookie"
+    });
+})(jQuery);
+</script>
